07d18761394dacea4b7e06727fb1994f67ce68fc
wn <csignore@whril.com>
2022-10-05 03:42:01 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Dead Money</title>
+        <meta name="description" content="Are we in a recession?" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <a
+          href="https://www.youtube.com/channel/UCCy9pBVQn1yVm5y3_mb4XrQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            <Image src="/dm-logo.png" alt="Dead Money Logo" width={250} height={125} />
+        </a>
+      </main>
+    </div>
+  )
+}
+
+export default Home
