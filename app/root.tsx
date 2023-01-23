97ae3edd8c997b777d6503245538186f345c4cc0
wn <csignore@whril.com>
2023-01-23 09:31:33 -0500

    x

--- app/root.tsx
+++ app/root.tsx
@@ -28,7 +28,7 @@ export const links: LinksFunction = () => {
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "Dead Money",
-  description: "Deadly irrelevant news from Wallstreetbets to Sand Hill Road. Join 300,000+ degenerates who can't lose.",
+  description: "Deadly irrelevant news from Wallstreetbets to Sand Hill Road. Join 300,000+ degenerates that can't lose.",
   viewport: "width=device-width,initial-scale=1",
 });
 
