19209ed76fc86d6e8c16b7a0354202f781c954b7
wn <csignore@whril.com>
2023-02-16 22:34:32 -0500

    l

--- .netlify/functions-internal/server.js
+++ .netlify/functions-internal/server.js
@@ -71,7 +71,7 @@ var import_jsx_dev_runtime2 = require("react/jsx-dev-runtime"), links = () => [
 ], meta = () => ({
   charset: "utf-8",
   title: "Dead Money",
-  description: "Deadly irrelevant news from Wallstreetbets to Sand Hill Road. Join 300,000+ degenerates that can't lose.",
+  description: "Deadly irrelevant news from wallstreetbets to Sand Hill Road. Sign up to our FREE newsletter and join 500,000+ degens that cant lose.",
   viewport: "width=device-width,initial-scale=1"
 });
 function App() {
@@ -132,23 +132,16 @@ __export(routes_exports, {
 });
 
 // public/img/dm2.png
-var dm2_default = "/build/_assets/dm2-XN76CP3U.png";
+var dm2_default = "/build/_assets/dm2-6EE2QE4W.png";
 
 // app/routes/index.tsx
 var import_jsx_dev_runtime3 = require("react/jsx-dev-runtime");
 function Index() {
-  return /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "container", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "inner-container", children: [
-    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("img", { src: dm2_default }, void 0, !1, {
-      fileName: "app/routes/index.tsx",
-      lineNumber: 7,
-      columnNumber: 9
-    }, this),
-    /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("p", { children: "(invite only)" }, void 0, !1, {
-      fileName: "app/routes/index.tsx",
-      lineNumber: 8,
-      columnNumber: 9
-    }, this)
-  ] }, void 0, !0, {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "container", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("div", { className: "inner-container", children: /* @__PURE__ */ (0, import_jsx_dev_runtime3.jsxDEV)("img", { src: dm2_default }, void 0, !1, {
+    fileName: "app/routes/index.tsx",
+    lineNumber: 7,
+    columnNumber: 9
+  }, this) }, void 0, !1, {
     fileName: "app/routes/index.tsx",
     lineNumber: 6,
     columnNumber: 7
@@ -160,7 +153,7 @@ function Index() {
 }
 
 // server-assets-manifest:@remix-run/dev/assets-manifest
-var assets_manifest_default = { version: "36440b73", entry: { module: "/build/entry.client-3Z3BMBC7.js", imports: ["/build/_shared/chunk-TY4X4K57.js", "/build/_shared/chunk-EETRBLDB.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-MDVMUGWN.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-5XHIQ3L2.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, url: "/build/manifest-36440B73.js" };
+var assets_manifest_default = { version: "a09bc44f", entry: { module: "/build/entry.client-3Z3BMBC7.js", imports: ["/build/_shared/chunk-TY4X4K57.js", "/build/_shared/chunk-EETRBLDB.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-WO3TIT4A.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 }, "routes/index": { id: "routes/index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/index-OBIYH3JW.js", imports: void 0, hasAction: !1, hasLoader: !1, hasCatchBoundary: !1, hasErrorBoundary: !1 } }, cssBundleHref: void 0, url: "/build/manifest-A09BC44F.js" };
 
 // server-entry-module:@remix-run/dev/server-build
 var assetsBuildDirectory = "public/build", future = { unstable_cssModules: !1, unstable_cssSideEffectImports: !1, unstable_vanillaExtract: !1, v2_errorBoundary: !1, v2_meta: !1, v2_routeConvention: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
