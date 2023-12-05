dee0969216fb8c9832f2119178034f8e526ba1d9
wn <csignore@whril.com>
2023-12-05 13:21:06 -0500

    xx

--- app/root.tsx
+++ app/root.tsx
@@ -27,7 +27,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Jeff Amzn",
+  title: "JEFF AMZN",
   description: "The newsletter for the next Buffett or Bezos.",
   viewport: "width=device-width,initial-scale=1",
 });
